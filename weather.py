17caa03feeae2c8dd0bfca8fc0a8bd5727d30c39
neozumm <neo.ivan92@gmail.com>
2019-09-25 21:38:29 +0300

    add changes

--- weather.py
+++ weather.py
@@ -3,6 +3,10 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+@app.route('/<string:mode>/')
+def weather_c(mode):
+    city = request.args.get('city')
+    return "%s weather for city %s" % (mode, city)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
