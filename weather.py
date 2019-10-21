e6341d8f823e6161640746a3f89fceb0713a7778
Vagrant Default User <vagrant@buster.localdomain>
2019-10-21 13:20:32 +0300

    outside host fix and url fixes

--- weather.py
+++ weather.py
@@ -13,23 +13,24 @@ api = js["key"]
 def weather_c(mode):
     city = request.args.get('city')
     dt = request.args.get('dt')
-    if mode == 'current':
-        r = requests.get(
-            'http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&APPID=%s' %
-            (city, api))
-        resp = r.json()
-        return "City: %s, unit: celsius, temperature: %s, weather: %s" % (
-            resp["name"], resp["main"].get("temp"), resp["weather"][0]["main"])
-    elif mode == 'forecast':
-        r = requests.get(
-            'http://api.openweathermap.org/data/2.5/forecast?q=%s&units=metric&APPID=%s' %
-            (city, api))
-        resp = r.json()
-        print(resp["list"][0]["dt"])
-        for i in resp["list"]:
-            if (i["dt"] == dt):
-                return i
-            return 'dt not found'
+    if city % mode:
+        if mode == 'current':
+            r = requests.get(
+                'http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&APPID=%s' %
+                (city, api))
+            resp = r.json()
+            return "City: %s, unit: celsius, temperature: %s, weather: %s" % (
+                resp["name"], resp["main"].get("temp"), resp["weather"][0]["main"])
+        elif mode == 'forecast' & dt:
+            r = requests.get(
+                'http://api.openweathermap.org/data/2.5/forecast?q=%s&units=metric&APPID=%s' %
+                (city, api))
+            resp = r.json()
+            print(resp["list"][0]["dt"])
+            for i in resp["list"]:
+                if (i["dt"] == dt):
+                    return i
+                return 'dt not found'
     else:
         return 'wrong url'
 
@@ -38,4 +39,4 @@ if __name__ == '__main__':
     if os.getenv('PORT'):
         app.run(debug=True, port=os.getenv('PORT'))
     else:
-        app.run(debug=True, port=5000)
+        app.run(debug=True, port=5000, host='0.0.0.0')
