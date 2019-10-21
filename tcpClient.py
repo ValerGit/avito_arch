306d97fb45d77a5f6a15cdd08a11231209b55f03
neozumm <neo.ivan92@gmail.com>
2019-10-21 23:15:04 +0300

    finished tcp

--- tcpClient.py
+++ tcpClient.py
@@ -5,11 +5,19 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 addr=(TCP_IP, TCP_PORT)
 BUFFER_SIZE = 64
-msg = "Hello, World!"
-MESSAGE =msg.encode()
 s = socket.create_connection(addr)
-s.sendall(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+while 1:
+    guess = input("guess the number: ")
+    s.sendall(guess.encode())
+    data = s.recv(BUFFER_SIZE)
+    if data:
+        dataDecoded=data.decode()
+        print ("server response: ", dataDecoded)
+        if dataDecoded=="correct!":
+            print ("congratualtions! you won!")
+            break
+    else: 
+        print ("damaged data recieved, exiting")
+        break
 s.close()
 
-print ("received data:", data.decode())
--- tcpServer.py
+++ tcpServer.py
@@ -1,18 +1,39 @@
 import socket
 import sys
-
+import random
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 64
 addr = (TCP_IP, TCP_PORT)
 s = socket.create_server(addr)
 s.listen(1)
-
+random.seed()
+secretNumber = random.randint(1,100)
 conn, addr = s.accept()
 print ("Connection address:", addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print ("received data:", data.decode())
-    conn.sendall(data)  # echo
+    decodedMsg = data.decode()
+    splitMsg = decodedMsg.split(" ")
+    print ("data split:", splitMsg)
+    if len(splitMsg) > 2:
+        print ("wrong data, recieved more")
+        conn.sendall("wrong data, recieved more".encode())
+    else:
+        if splitMsg[0] == "guess":
+            if splitMsg[1].isdigit():
+                if int(splitMsg[1]) < secretNumber:
+                    conn.sendall("less".encode())
+                elif int(splitMsg[1]) == secretNumber:
+                    conn.sendall("correct!".encode())
+                elif int(splitMsg[1]) > secretNumber:
+                    conn.sendall("more".encode())
+            else:
+                print ("wrong data, not a digit")
+                conn.sendall("wrong data, not a digit".encode())
+        else:
+            print ("wrong data, not guess")
+            conn.sendall("wrong data, not guess".encode())
 conn.close()
+s.close()
